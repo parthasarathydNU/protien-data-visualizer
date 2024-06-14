b38c2caafeed7e32b05ccc743f42148e0fd26799
Dhruv Parthasarathy <parthasarathy.d@northeastern.edu>
2024-06-14 17:34:15 -0400

    Pass in all table infromation along with the queries for context

--- package/api/lang_folder/agents.py
+++ package/api/lang_folder/agents.py
@@ -1,3 +1,4 @@
+from lang_folder.database import db
 from lang_folder.few_shot_examples import few_shot_examples
 from lang_folder.chains import classification_chain, conversation_chain, table_info_dynamic_few_shot_system_prompt_chain, follow_up_questions_chain
 # Function to classify a given input string
@@ -18,14 +19,14 @@ def get_few_shot_examples():
 def get_ai_response_for_conversation(conversation):
     formatted_conversation = [('ai' if entry["role"] == 'assistant' else 'human', entry["content"]) for entry in conversation]
     print(f"formatted_conversation {formatted_conversation}")
-    result = conversation_chain.invoke({"table_info" : "" , "conversation" : formatted_conversation})
+    result = conversation_chain.invoke({"table_info" : db.table_info , "conversation" : formatted_conversation})
     print(f"Result from ai {result}")
     return result
 
 def get_follow_up_questions_from_ai(conversation):
     formatted_conversation = [('ai' if entry["role"] == 'assistant' else 'human', entry["content"]) for entry in conversation]
     print(f"formatted_conversation {formatted_conversation}")
-    result = follow_up_questions_chain.invoke({"table_info" : "" , "conversation" : formatted_conversation})
+    result = follow_up_questions_chain.invoke({"table_info" : db.table_info , "conversation" : formatted_conversation})
     print(f"Result from ai {result}")
     return result
 
