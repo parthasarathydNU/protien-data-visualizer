931d91a266506155d4ad66ee90a44cdf1206396b
Dhruv Parthasarathy <parthasarathy.d@northeastern.edu>
2024-06-28 18:02:02 -0400

    added type checks to query and get follow up end point

--- package/dashboards/protein-dashboard/src/api.ts
+++ package/dashboards/protein-dashboard/src/api.ts
@@ -1,33 +1,54 @@
-import { apiRequest } from './utils/apiUtils';
-
-export const getAIResponse = async (payload: any) => {
-  return apiRequest<any>({ method: 'post', url: 'query/', payload });
+import { apiRequest } from "./utils/apiUtils";
+export enum MessageRolesEnum {
+  assistant,
+human,
+}
+export type Message = { role: MessageRolesEnum; content: string };
+export type AIRequestPayload = {
+  query: string;
+  context: Message[];
 };
 
-export const getFollowUpQuestions = async (payload: any) => {
-  return apiRequest<any>({ method: 'post', url: 'query_followup/', payload });
+export type FollowUpQuestionsResponse = {
+  follow_up_questions: string[]
+}
+
+export const getAIResponse = async (payload: AIRequestPayload) => {
+  return apiRequest<any>({ method: "post", url: "query/", payload });
+};
+
+export const getFollowUpQuestions = async (payload: AIRequestPayload) : Promise<FollowUpQuestionsResponse> => {
+  return apiRequest<any>({ method: "post", url: "query_followup/", payload });
 };
 
 export const fetchProteins = async () => {
-  return apiRequest<any[]>({ method: 'get', url: 'proteins/' });
+  return apiRequest<any[]>({ method: "get", url: "proteins/" });
 };
 
 export const fetchProteinCalculations = async (entry: string) => {
-  return apiRequest<any>({ method: 'get', url: `get_protein_data/${entry}` });
+  return apiRequest<any>({ method: "get", url: `get_protein_data/${entry}` });
 };
 
 export const fetchProtein = async (entry: string) => {
-  return apiRequest<any>({ method: 'get', url: `proteins/${entry}` });
+  return apiRequest<any>({ method: "get", url: `proteins/${entry}` });
 };
 
 export const createProtein = async (protein: any) => {
-  return apiRequest<any>({ method: 'post', url: 'proteins/', payload: protein });
+  return apiRequest<any>({
+    method: "post",
+    url: "proteins/",
+    payload: protein,
+  });
 };
 
 export const updateProtein = async (entry: string, protein: any) => {
-  return apiRequest<any>({ method: 'put', url: `proteins/${entry}`, payload: protein });
+  return apiRequest<any>({
+    method: "put",
+    url: `proteins/${entry}`,
+    payload: protein,
+  });
 };
 
 export const deleteProtein = async (entry: string) => {
-  return apiRequest<any>({ method: 'delete', url: `proteins/${entry}` });
+  return apiRequest<any>({ method: "delete", url: `proteins/${entry}` });
 };
