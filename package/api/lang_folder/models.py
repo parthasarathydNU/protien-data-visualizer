b7b2072f24d3c4d415a4f13acc8615dce3a9c3d3
Dhruv Parthasarathy <parthasarathy.d@northeastern.edu>
2024-06-28 17:49:40 -0400

    defined follow up questions model

--- package/api/lang_folder/models.py
+++ package/api/lang_folder/models.py
@@ -6,6 +6,11 @@ class Table(BaseModel):
     """Table in SQL database."""
     name: str = Field(description="Name of table in SQL database.")
 
+# Model for followup queries 
+class FollowUpQueries(BaseModel):
+    """List of questions that the user can ask as a follow up to the bot"""
+    follow_up_questions : List[str] = Field(description="List of questions that the user can ask as a follow up to the bot")
+
 # Define the TableList model
 class TableList(BaseModel):
     """List of tables that are relevant to the given input"""
